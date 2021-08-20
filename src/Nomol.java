e8f4866734ad63ca8592d1b40e169af1611955b5
wangjian <wangj2115@163.com>
2021-08-20 19:20:56 +0800

    Dev第一次添加

---
+++ src/Nomol.java
@@ -0,0 +1,14 @@
+/**
+ * @Author Wang
+ * @Create 2021/8/20  19:18
+ * @Version 1.0
+ */
+public class Nomol {
+    public static void main(String[] args) {
+        System.out.printf("傻逼");
+        System.out.printf("傻逼");
+        System.out.printf("傻逼");
+        System.out.printf("************************");
+        System.out.printf("Dev分支");
+    }
+}
